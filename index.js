99f3891b3bb24deee6e1cdbdc743ec48b60f453d
Md. Abu Sayeed Mondal <sayeedmondal1412@gmail.com>
2021-12-02 18:06:37 +0600

    added primary files

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+//require path
+const path = require("path");
+
+//set a cors middleware
+app.use(cors());
+
+//set static folder to public with path
+app.use(express.static(path.join(__dirname, "public")));
+
+//set a index route
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "static/index.html"));
+});
+
+//set a port
+const port = process.env.PORT || 5000;
+
+//start server
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
