52293757212f3cdac23254bc41fde1cc420f8815
Md. Abu Sayeed Mondal <sayeedmondal1412@gmail.com>
2021-12-06 12:52:37 +0600

    check with fs

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require("cors");
 //require path
 const path = require("path");
+const fs = require("fs");
 
 //set a cors middleware
 app.use(cors());
@@ -12,8 +13,17 @@ app.use(cors());
 
 //set a index route
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "node", "app", "index.html"));
-    // res.send("hello");
+    //check fs stat on index.html
+    fs.stat(path.join(__dirname, "node", "app", "index.html"), (err, stats) => {
+        if (err) {
+            console.log(err);
+            return res.send({ Error: err });
+        }
+        //if stat is ok, send index.html
+
+        return res.sendFile(path.join(__dirname, "node", "app", "index.html"));
+        // res.send("hello");
+    });
 });
 
 //set a port
