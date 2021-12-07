9a48535ff5d52c9c4c3e2758e35eb080c4815df6
Md. Abu Sayeed Mondal <sayeedmondal1412@gmail.com>
2021-12-07 09:39:06 +0600

    upload image and access it using multer

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const cors = require("cors");
 //require path
 const path = require("path");
 const fs = require("fs");
+//import multer
+const multer = require("multer");
 
 //set a cors middleware
 app.use(cors());
@@ -11,10 +13,38 @@ app.use(cors());
 //set static folder to public with path
 app.use(express.static("/node/app"));
 
+// //set static folder with __dirname, 'node' and 'app' folder with path.join
+// app.use(express.static(path.join(__dirname, "node", "app")));
+
 //set a index route
 app.get("/", (req, res) => {
     return res.sendFile(path.join(__dirname, "static", "index.html"));
     // res.send("hello");
+});
+
+//multer storage to "/node/app/" folder with filename with date
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "./node/app/");
+    },
+    filename: (req, file, cb) => {
+        cb(null, new Date().toISOString() + file.originalname);
+    },
+});
+
+//upload file with multer
+const upload = multer({
+    storage: storage,
+}).single("file");
+
+//a post request to upload a image using multer to the folder "/node/app/"
+app.post("/upload", upload, (req, res) => {
+    //access the file with fs.readFileSync
+    const file = fs.readFileSync(req.file.path);
+    //set the content type
+    res.set("Content-Type", req.file.mimetype);
+    //send the file
+    res.send(file);
 });
 
 //set a port
